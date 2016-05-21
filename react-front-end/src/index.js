42070c83022dce7c50320a4804e671ddefe1d3d5
Ksenia <gueletina.ksenia@gmail.com>
2016-05-21 13:10:18 -0400

    added react-socket

--- react-front-end/src/index.js
+++ react-front-end/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Socket from 'react-socket';
 
 ReactDOM.render(<App />, document.getElementById('root'));
